5b9dafd4833f703f7f40e45a3cb5c5056f7e480e
Oussama Ben Lagha <o.benlagha@reply.de>
2020-10-16 16:39:21 +0200

    Add tests to PreProd stage

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -3,6 +3,7 @@ import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
 import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
+import { ShellScriptAction } from '@aws-cdk/pipelines';
 
 /**
  * The stack that defines the application pipeline
@@ -40,8 +41,25 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
 
     // This is where we add the application stages
     // ...
-    pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+    //pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+    //    env: { account: '552701094143', region: 'eu-central-1' }
+    //  }));
+
+      const preprod = new CdkpipelinesDemoStage(this, 'PreProd', {
         env: { account: '552701094143', region: 'eu-central-1' }
+      });
+      const preprodStage = pipeline.addApplicationStage(preprod);
+      preprodStage.addActions(new ShellScriptAction({
+        actionName: 'TestService',
+        useOutputs: {
+          // Get the stack Output from the Stage and make it available in
+          // the shell script as $ENDPOINT_URL.
+          ENDPOINT_URL: pipeline.stackOutput(preprod.urlOutput),
+        },
+        commands: [
+          // Use 'curl' to GET the given URL and fail if it returns an error
+          'curl -Ssf $ENDPOINT_URL',
+        ],
       }));
 
       pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, 'Prod', {
