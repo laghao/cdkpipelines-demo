a0b9fea8506cac3984a8e97192b484a09683e7a1
Oussama Ben Lagha <o.benlagha@reply.de>
2020-10-16 17:30:46 +0200

    Initial deployment of our empty pipeline

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoStage } from '../lib/cdkpipelines-demo-stage';
 
 const app = new App();
 
@@ -8,4 +9,8 @@ new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   env: { account: '552701094143', region: 'eu-central-1' },
 });
 
+new CdkpipelinesDemoStage(app, 'Dev', {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  });
+  
 app.synth();
